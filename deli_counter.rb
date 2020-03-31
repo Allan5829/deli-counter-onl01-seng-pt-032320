bd2a9c07538236927eeb2bdcb305e1c0e9d9c331
Allan Farca <allan.farca@gmail.com>
2020-03-31 03:19:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,8 @@ end
 
 def now_serving (katz_deli)
   if katz_deli.size == 0
-    puts "The line is currently empty."
+    puts "There is nobody waiting to be served!"
   else 
+    puts 
+  end
 end
