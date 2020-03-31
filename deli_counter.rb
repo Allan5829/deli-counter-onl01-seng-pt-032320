b4b2ef185514a9ccb6604e9520d4189925e65f18
Allan Farca <allan.farca@gmail.com>
2020-03-31 03:41:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def line(katz_deli)
 end
 
 def take_a_number (katz_deli, name)
-    katz_deli.push ("#{name}")
+    katz_deli.push (name)
     puts "Welcome, #{katz_deli}. You are number #{katz_deli.size} in line."
 end
 
